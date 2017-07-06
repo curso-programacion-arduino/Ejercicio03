44052d8ee030002409f8f6f4282ef78d2a645425
Enrique Crespo <jecrespom@gmail.com>
2017-07-06 08:23:02 +0200

    Constantes y Variables

---
+++ Ejercicio03-Enrique_Pasos/Ejercicio03-Enrique_Pasos.ino
@@ -0,0 +1,18 @@
+#define PIN_BOTON_A 2
+#define PIN_BOTON_B 3
+
+//Variables globales
+int pulsos_a = 0;
+int pulsos_b = 0;
+boolean anterior_a;
+boolean anterior_b;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
