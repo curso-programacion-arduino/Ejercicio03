15cb7d94d8f1e701b4f822b99624bbea8b66f66b
Raiden7 <pascu1987@hotmail.com>
2017-07-06 17:40:50 +0200

    commit 1

---
+++ Ejercicio03-David/Ejercicio03-David.ino
@@ -0,0 +1,48 @@
+#define PIN_BOTON_A 2
+#define PIN_BOTON_B 3
+
+//Variables globales
+int pulsaciones_A = 0;
+int pulsaciones_B = 0;
+int totalPulsaciones = 0;
+boolean estadoInicial_B;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(PIN_BOTON_A, INPUT_PULLUP);
+  pinMode(PIN_BOTON_B, INPUT_PULLUP);
+
+  //Guardo los estados inciales de los botones
+  estadoInicial_A = digitalRead(PIN_BOTON_A);
+  estadoInicial_B = digitalRead(PIN_BOTON_B);
+}
+
+void loop() {
+  boolean estadoActual_A = digitalRead(PIN_BOTON_A);
+  boolean estadoActual_B = digitalRead(PIN_BOTON_B);
+
+  if (estadoInicial_A != estadoActual_A) {
+    if (estadoActual_A == LOW) {
+      pulsaciones_A++;
+      imprimePulsaciones();
+    }
+  }
+ 
+  if (estadoInicial_B != estadoActual_B) {
+    if (estadoInicial_B == LOW) {  
+      pulsaciones_B++;
+      imprimePulsaciones();
+    }
+  }
+  estadoInicial_A = estadoInicial_A;
+  estadoInicial_B = estadoInicial_B;
+  totalPulsaciones++;
+  delay(90); 
+}
+
+void imprimePulsaciones() {
+  Serial.println("Contador A: " + (String)pulsaciones_A + "\tContador B: " + (String)pulsaciones_B);
+  Serial.println("Total pulsaciones: " + );
+}
+
