0d481b58d734dc03fe6fdb90390a15c5376e2892
Raiden7 <pascu1987@hotmail.com>
2017-07-06 17:45:38 +0200

    Solventado errores, cuenta bien y saca el numero de pulsaciones totales

--- Ejercicio03-David/Ejercicio03-David.ino
+++ Ejercicio03-David/Ejercicio03-David.ino
@@ -5,8 +5,8 @@
 int pulsaciones_A = 0;
 int pulsaciones_B = 0;
 int totalPulsaciones = 0;
+boolean estadoInicial_A;
 boolean estadoInicial_B;
-
 void setup() {
   Serial.begin(9600);
 
@@ -16,6 +16,7 @@ void setup() {
   //Guardo los estados inciales de los botones
   estadoInicial_A = digitalRead(PIN_BOTON_A);
   estadoInicial_B = digitalRead(PIN_BOTON_B);
+  Serial.println("Pulse por favor los botones para inicializar las cuentas");
 }
 
 void loop() {
@@ -25,6 +26,7 @@ void loop() {
   if (estadoInicial_A != estadoActual_A) {
     if (estadoActual_A == LOW) {
       pulsaciones_A++;
+      totalPulsaciones++;
       imprimePulsaciones();
     }
   }
@@ -33,16 +35,17 @@ void loop() {
     if (estadoInicial_B == LOW) {  
       pulsaciones_B++;
       imprimePulsaciones();
+      totalPulsaciones++;
     }
   }
   estadoInicial_A = estadoInicial_A;
   estadoInicial_B = estadoInicial_B;
-  totalPulsaciones++;
+  
   delay(90); 
 }
 
 void imprimePulsaciones() {
   Serial.println("Contador A: " + (String)pulsaciones_A + "\tContador B: " + (String)pulsaciones_B);
-  Serial.println("Total pulsaciones: " + );
+  Serial.println("Total pulsaciones: " + (String)totalPulsaciones);
 }
 
